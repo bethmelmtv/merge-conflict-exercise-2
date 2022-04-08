e275da5b9e59454bb40ff0a3560e976b2d59cee8
JeffAllison <itsjallison@gmail.com>
2022-04-08 14:20:56 -0700

    made a change to app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// import functions and grab DOM elements
+here is my change
 
 // let state
 
 // set event listeners 
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
