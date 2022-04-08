c3f18f60620c9b5f002191461014e1ffe4cd5ea2
Delaney Fogarty <delaney.fk@gmail.com>
2022-04-08 14:40:29 -0700

    delaney added a scary change

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // import functions and grab DOM elements
 
-// let state
+Delaney made a scary change.
 
-// set event listeners 
+// set event listeners
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
